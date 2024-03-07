67758104c4085045ed389afd334fec9cfbc910d8
joe mann <joe.mann@fdmgroup.com>
2024-03-07 10:45:33 +0800

    Add comments and refactor code

--- src/elevatorFiles/Clock.java
+++ src/elevatorFiles/Clock.java
@@ -1,33 +1,42 @@
 package elevatorFiles;
 
+/**
+ * The Clock class represents the simulation clock. It keeps track of the
+ * simulated time.
+ */
 public class Clock {
 
 	// Attribute
 	// Create a static time variable which increases forever as the simulation runs
 	private static int time = 0;
-	
 
-	// Add default constructor
-	
+	// Default constructor
 	public Clock() {
-		
+
 	}
-	
-	public static int getTime() {
-		int x = 0;
-		return Clock.time;
-	}
-	
 
 	/**
-	 * Method to increase simulated time
+	 * Gets the current simulated time.
+	 * 
+	 * @return The current simulated time.
+	 */
+	public static int getTime() {
+		return time;
+	}
+
+	/**
+	 * Method to increase simulated time by one unit.
 	 */
 	public static void tick() {
 		time++;
 	}
-	
+
+	/**
+	 * Method to increase simulated time by a specified duration.
+	 * 
+	 * @param duration The duration by which to increase the simulated time.
+	 */
 	public static void tick(int duration) {
-		time+=duration;
+		time += duration;
 	}
-	
 }
--- src/elevatorFiles/ManagerThread.java
+++ src/elevatorFiles/ManagerThread.java
@@ -1,19 +1,35 @@
 package elevatorFiles;
 
+/**
+ * The ManagerThread class represents a thread responsible for deploying
+ * elevators in the simulation.
+ */
 public class ManagerThread implements Runnable {
 	private Manager manager;
 
+	/**
+	 * Retrieves the manager associated with this thread.
+	 * 
+	 * @return The manager object.
+	 */
 	public Manager getManager() {
 		return manager;
 	}
 
+	/**
+	 * Sets the manager associated with this thread.
+	 * 
+	 * @param manager The manager object to set.
+	 */
 	public void setManager(Manager manager) {
 		this.manager = manager;
 	}
 
+	/**
+	 * Runs the thread to deploy elevators.
+	 */
 	@Override
 	public void run() {
-		manager.whichElevatorToGo();		
+		manager.deployElevators();
 	}
-	
 }
